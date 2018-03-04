328d593cf4fd1227b148495a46a73ee30c748b2e
huchunjia <huchunjia.sjtu@gmail.com>
2018-03-03 16:23:06 -0800

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -27,7 +27,7 @@ main() {
   CHECK_ZSH_INSTALLED=$(grep /zsh$ /etc/shells | wc -l)
   if [ ! $CHECK_ZSH_INSTALLED -ge 1 ]; then
     # printf "${YELLOW}Zsh is not installed!${NORMAL} Please install zsh first!\n"
-    sudo apt-get install zsh
+    sudo apt-get install -y zsh
     CHECK_ZSH_INSTALLED=$(grep /zsh$ /etc/shells | wc -l)
   fi
   if [ ! $CHECK_ZSH_INSTALLED -ge 1 ]; then
